45e105f0e76c53ef8c805527fd3d2bf9509548fc
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-25 08:46:44 -0300

    first commit - ms-pedidos

---
+++ ms-pedidos/src/main/java/com/github/vitorgarcia03/ms/pedidos/MsPedidosApplication.java
@@ -0,0 +1,13 @@
+package com.github.vitorgarcia03.ms.pedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsPedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsPedidosApplication.class, args);
+	}
+
+}
---
+++ ms-pedidos/src/test/java/com/github/vitorgarcia03/ms/pedidos/MsPedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.vitorgarcia03.ms.pedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
